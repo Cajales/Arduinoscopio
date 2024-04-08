d8de5003ec55cda049045f8aaa357fbc357621b6
Cajales <32377693+Cajales@users.noreply.github.com>
2024-04-08 14:14:39 -0400

    Add files via upload

---
+++ Lowbyte.ino
@@ -0,0 +1,20 @@
+int V=0;
+byte Vl;
+byte Vh;
+byte lb;
+int i;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(115200);
+}
+
+void loop() {
+  V= analogRead(A0);
+  lb=lowByte(0);
+  Serial.write(lb);
+  Vh=highByte(V<<3);
+  Serial.write(Vh|0b11100000);
+  Vl=(lowByte(V))&0b00011111;   
+  Serial.write(Vl|0b01100000);
+
+}
